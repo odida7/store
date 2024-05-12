ff65d995ba8e21639de9947599ae59c6ea26a692
odida7 <danielodida@yahoo.com>
2024-05-12 21:23:16 +0300

    add

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,10 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const ProductCard = ({product}: {product: ProductType}) => {
   return (
+   <Link
+    href={`/products/${product._id}`}
+    className="w-[220px] flex flex-col gap-2"
+  >
+    <Image
+      src={product.media[0]}
+      alt="product"
+      width={250}
+      height={300}
+      className="h-[250px] rounded-lg object-cover"
+    />
     <div>
-      pro
+      <p className="text-base-bold">{product.title}</p>
+      <p className="text-small-medium text-grey-2">{product.category}</p>
     </div>
+    <div className="flex justify-between items-center">
+      <p className="text-body-bold">${product.price}</p>
+     {/* <HeartFavorite product={product} updateSignedInUser={updateSignedInUser} />*/}
+    </div>
+  </Link>
   )
 }
 
