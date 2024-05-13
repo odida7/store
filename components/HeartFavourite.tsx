841b65be38733843511707204753e76e1ebfca2b
odida7 <danielodida@yahoo.com>
2024-05-13 23:13:48 +0300

    add

---
+++ components/HeartFavourite.tsx
@@ -0,0 +1,12 @@
+import { HeartIcon } from 'lucide-react'
+import React from 'react'
+
+const HeartFavourite = () => {
+  return (
+    <div>
+      <HeartIcon/>
+    </div>
+  )
+}
+
+export default HeartFavourite
