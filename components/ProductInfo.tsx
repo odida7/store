7f870931f3a174ceb43cc08778fb2c5c2beca279
odida7 <danielodida@yahoo.com>
2024-06-15 16:12:48 +0300

    add

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const ProductInfo = () => {
+export default function ProductInfo() {
   return (
     <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid eius, tempore adipisci, porro, nesciunt qui facere libero saepe officia incidunt blanditiis error id praesentium omnis ea nam in obcaecati maxime.
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, excepturi sunt voluptatem blanditiis alias rerum necessitatibus consequuntur molestiae possimus quas officiis non laudantium, iusto illo minima dignissimos sed quae dolores.</h1>
     </div>
   )
 }
-
-export default ProductInfo
